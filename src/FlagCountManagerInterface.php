cb3fd84073356ad56b9279ef6850a65fefd957d7
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2015-05-13 20:19:44 +0200

    Issue #2476499 by joachim, martin107, socketwench: Added detail to the documentation of the flag count API.

--- src/FlagCountManagerInterface.php
+++ src/FlagCountManagerInterface.php
@@ -19,47 +19,76 @@ use Drupal\flag\FlagInterface;
 interface FlagCountManagerInterface {
 
   /**
-   * Get flag counts for all flags on a node.
+   * Gets flag counts for all flags on an entity.
+   *
+   * Provides a count of all the flaggings for a single entity. Instead
+   * of a single response, this method returns an array of counts keyed by
+   * the flag ID:
+   *
+   * @code
+   * array(
+   *   my_flag => 42
+   *   another_flag => 57
+   * );
+   * @endcode
    *
    * @param $entity
    *   The entity.
    *
-   * @return
-   *   The flag count with the flag names as array keys.
+   * @return array
+   *   An array giving the counts of all flaggings on the entity. The flag IDs
+   *   are the keys and the counts for each flag the values. Note that flags
+   *   that have no flaggings are not included in the array.
    */
   public function getCounts(EntityInterface $entity);
 
   /**
-   * Get the count of flags for a certain entity.
+   * Gets the count of flaggings for the given flag.
+   *
+   * For example, if you have an 'endorse' flag, this method will tell you how
+   * many endorsements have been made, rather than how many things have been
+   * endorsed.
    *
    * @param \Drupal\flag\FlagInterface $flag
    *   The flag.
    *
-   * @return
-   *   The flag count with the flag name and entity type as the array key.
+   * @return int
+   *   The number of flaggings for the flag.
    */
   public function getEntityCounts(FlagInterface $flag);
 
   /**
-   * Get the total count of items flagged within a flag.
+   * Gets the count of entities flagged by the given flag.
+   *
+   * For example, with a 'report abuse' flag, this returns the number of
+   * entities that have been reported, not the total number of reports. In other
+   * words, an entity that has been reported multiple times will only be counted
+   * once.
    *
    * @param \Drupal\flag\FlagInterface
    *   The flag for which to retrieve a flag count.
    * @param $reset
-   *   (optional) Reset the internal cache and execute the SQL query another time.
+   *   (optional) Reset the internal cache and execute the SQL query another
+   *   time.
+   *
+   * @return int
+   *   The number of entities that are flagged with the flag.
    */
   public function getTotals(FlagInterface $flag, $reset = FALSE);
 
   /**
-   * Get the user's flag count.
+   * Gets the count of the flaggings made by a user with a flag.
+   *
+   * For example, with a 'bookmarks' flag, this returns the number of bookmarks
+   * a user has created.
    *
    * @param \Drupal\flag\FlagInterface $flag
    *   The flag.
    * @param \Drupal\Core\Session\AccountInterface $user
-   *   The user object.
+   *   The account.
    *
-   * @return
-   *   The flag count with the flag name and the uid as the array key.
+   * @return int
+   *   The number of flaggings for the given flag and user.
    */
   public function getUserCounts(FlagInterface $flag, AccountInterface $user);
 
