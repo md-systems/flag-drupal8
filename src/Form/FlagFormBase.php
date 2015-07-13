d1a5dac2953bea3efc7f147c62679fdd400f7786
joshi.rohit100 <joshi.rohit100@2504832.no-reply.drupal.org>
2015-07-13 08:23:00 +0100

    Issue #2528600 by joshi.rohit100: Updated for core change from EntityFormInterface::validate() to validateForm().

--- src/Form/FlagFormBase.php
+++ src/Form/FlagFormBase.php
@@ -247,10 +247,10 @@ abstract class FlagFormBase extends EntityForm {
   }
 
   /**
-   * Overrides Drupal\Core\Entity\EntityFormController::validate().
+   * Overrides Drupal\Core\Entity\EntityFormController::validateForm().
    */
-  public function validate(array $form, FormStateInterface $form_state) {
-    parent::validate($form, $form_state);
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
 
     // @todo Move this to the validation method for the confirm form plugin
     $flag = $this->entity;
