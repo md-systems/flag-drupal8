834bbfb60e1fa5d5dd0f329d5c13f9b4d9e02003
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2015-12-05 08:46:27 +0000

    Issue #2581505 by joachim: Changed flag operations to be on admin paths.

--- src/Tests/AdminUITest.php
+++ src/Tests/AdminUITest.php
@@ -164,7 +164,7 @@ class AdminUITest extends FlagTestBase {
     $this->drupalGet('admin/structure/flags');
     $this->assertText(t('enabled'));
 
-    $this->drupalPostForm('flag/disable/' . $this->flagId, [], $this->t('Disable'));
+    $this->drupalPostForm('admin/structure/flags/manage/' . $this->flagId . '/disable', [], $this->t('Disable'));
     $this->assertResponse(200);
 
     $this->drupalGet('admin/structure/flags');
@@ -181,7 +181,7 @@ class AdminUITest extends FlagTestBase {
     $this->drupalGet('admin/structure/flags');
     $this->assertText(t('disabled'));
 
-    $this->drupalPostForm('flag/enable/' . $this->flagId, [], $this->t('Enable'));
+    $this->drupalPostForm('admin/structure/flags/manage/' . $this->flagId . '/enable', [], $this->t('Enable'));
     $this->assertResponse(200);
 
     $this->drupalGet('admin/structure/flags');
@@ -207,7 +207,7 @@ class AdminUITest extends FlagTestBase {
     $this->assertEqual(count($ids_before), 1, "The flag has one flagging.");
 
     // Go to the reset form for the flag.
-    $this->drupalGet('flag/reset/' . $this->flag->id());
+    $this->drupalGet('admin/structure/flags/manage/' . $this->flag->id() . '/reset');
 
     $this->assertText($this->t('Are you sure you want to reset the Flag'));
 
